d89edd9983a542b03e9d99ef3dc4ff045cb78b4d
creativeprashant007 <prashanttimalsina18@gmail.com>
2023-12-19 13:23:49 +0000

    fix:game screen code

--- lib/screens/tic_tac_toe_screen.dart
+++ lib/screens/tic_tac_toe_screen.dart
@@ -37,16 +37,15 @@ class _TicTacToeState extends State<TicTacToe> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Container(
-          child: const Text(
-            'Tic Tac Toe',
-            style: TextStyle(
-                fontFamily: 'Quicksand',
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 255, 255, 255)),
-          ),
+        title: const Text(
+          'Tic Tac Toe',
+          style: TextStyle(
+              fontFamily: 'Quicksand',
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 255, 255, 255)),
         ),
+
         backgroundColor: Colors.deepOrange,
         centerTitle: true, // Center the title
       ),
@@ -83,61 +82,58 @@ class _TicTacToeState extends State<TicTacToe> {
             Center(
               child: BackdropFilter(
                 filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      // Blurred Menu Content
-                      TextButton(
-                        onPressed: () {
-                          restartGame();
-                        },
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.green),
-                          foregroundColor:
-                              MaterialStateProperty.all<Color>(Colors.white),
-                          padding:
-                              MaterialStateProperty.all<EdgeInsetsGeometry>(
-                            const EdgeInsets.symmetric(
-                                horizontal: 20, vertical: 10),
-                          ),
-                          shape: MaterialStateProperty.all<OutlinedBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                              side: const BorderSide(color: Colors.green),
-                            ),
-                          ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Blurred Menu Content
+                    TextButton(
+                      onPressed: () {
+                        restartGame();
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.green),
+                        foregroundColor:
+                            MaterialStateProperty.all<Color>(Colors.white),
+                        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                          const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
                         ),
-                        child: const Text(
-                          'Restart Game',
-                          style: TextStyle(
-                              fontFamily: 'Quicksand',
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold),
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            side: const BorderSide(color: Colors.green),
+                          ),
                         ),
                       ),
-                     const  SizedBox(height: 20),
-
-                      Container(
-                        padding:const EdgeInsets.all(15.0),
-                        decoration: BoxDecoration(
-                          color: winBackgroundColor,
-                          borderRadius: BorderRadius.circular(
-                              8), // Optional: Set border radius
-                        ),
-                        child: Text(
-                          'Player ${currentPlayer == 'X' ? 'O' : 'X'} Wins 🎉!',
-                          style: const TextStyle(
+                      child: const Text(
+                        'Restart Game',
+                        style: TextStyle(
                             fontFamily: 'Quicksand',
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 255, 255, 255),
-                          ),
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+
+                    Container(
+                      padding: const EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                        color: winBackgroundColor,
+                        borderRadius: BorderRadius.circular(
+                            8), // Optional: Set border radius
+                      ),
+                      child: Text(
+                        'Player ${currentPlayer == 'X' ? 'O' : 'X'} Wins 🎉!',
+                        style: const TextStyle(
+                          fontFamily: 'Quicksand',
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 255, 255, 255),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
