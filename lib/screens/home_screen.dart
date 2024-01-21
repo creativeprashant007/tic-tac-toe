bcc63bfda0cb6067caf747adf3aa87b6678ab8a5
creativeprashant007 <prashanttimalsina18@gmail.com>
2024-01-21 23:53:37 +0000

    fix:home screen routing

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:tic_tac_toe/model/home_components.dart';
 
 class Home extends StatefulWidget {
@@ -11,13 +12,13 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List<HomeComponents> homeComp = [
     HomeComponents(
-      title: "New Game",
+      title: "Single Player",
       callback: () {
         // Navigator.pushNamed(context, '/tic_tac_toe_screen');
       },
     ),
     HomeComponents(
-      title: "Statistics",
+      title: "Multi Players",
       callback: () {},
     ),
     HomeComponents(
@@ -37,9 +38,23 @@ class _HomeState extends State<Home> {
           itemBuilder: (context, index) {
             var homeItem = homeComp[index];
             return ListTile(
-              title: Text(homeItem.title!),
-              onTap: () => Navigator.pushNamed(context, '/tic_tac_toe_screen'),
-            );
+                title: Text(homeItem.title!),
+                onTap: () {
+                  if (index == 0) {
+                    Navigator.of(context).pushNamed('tic_tac_toe_screen');
+                  } else if (index == 1) {
+                    Navigator.of(context).pushNamed("single_player");
+                  } else {
+                    Fluttertoast.showToast(
+                        msg: "Back 2 more time to close the app",
+                        toastLength: Toast.LENGTH_SHORT,
+                        gravity: ToastGravity.CENTER,
+                        timeInSecForIosWeb: 1,
+                        backgroundColor: Colors.red,
+                        textColor: Colors.white,
+                        fontSize: 16.0);
+                  }
+                });
           },
           separatorBuilder: (_, __) => const SizedBox(
                 height: 10,
